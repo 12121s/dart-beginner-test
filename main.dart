9b1bb0198cdb5970fc9f81c351bbc15fbc35b1f8
mr_illi <kimri42@naver.com>
2023-05-01 15:27:27 +0900

    late 변수 테스트

--- main.dart
+++ main.dart
@@ -18,5 +18,9 @@ void main() {
   /* # 1.4 Final Variables 
   final name = 'illi'; // 수정 불가 */
 
+  /* # 1.5 Late Variables */
+  late final String name;
+  name = 'illi'; // 나중에 초기화
+  print(name);
 
 }
