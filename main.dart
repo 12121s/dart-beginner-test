a6cd5e11ab7006d0be76f27e39bc2e369de22ae8
mr_illi <kimri42@naver.com>
2023-05-01 15:20:32 +0900

    Null Safety 테스트

--- main.dart
+++ main.dart
@@ -1,8 +1,17 @@
 void main() {
+  /* # 1.2 Dynamic Type
   dynamic variable; // var variable 가능
   variable = 'name';
   variable = 1;
   variable = true;
 
-  print(variable);
+  print(variable);*/
+
+  /* # 1.3 Nullable Variables */
+  String? name = 'illi';
+  name = null;
+  /* if (name != null) {
+    print(name.isNotEmpty());
+  } */
+  print(name?.isNotEmpty);
 }
