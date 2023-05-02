50afedbace4c3874a493282e0da15300eb4a594c
mr_illi <kimri42@naver.com>
2023-05-02 12:08:50 +0900

    String Interpolation 학습

--- main2.dart
+++ main2.dart
@@ -1,5 +1,5 @@
 void main() {
-  /* # 2.1 Lists */
+  /* # 2.1 Lists
   var giveMeFive = true;
 
   // var numbers와 동일
@@ -14,5 +14,19 @@ void main() {
     */
     if (giveMeFive) 5,
   ];
-  print(numbers);
+  print(numbers); */
+
+  /* # 2.2 String Interpolation: 텍스트에 변수 추가하는 방법 */
+
+  // 1) 변수가 이전에 존재할 때
+  var name = 'illi';
+  var greet = 'hello, $name';
+
+  print(greet);
+
+  // 2) 계산식을 추가할 때
+  var age = 10;
+  var intro = "I'm ${age * 2}"; // 'I\'m ${age * 2}'
+
+  print(intro);
 }
