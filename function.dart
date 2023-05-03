505ea8bee1a476d2b99f02fc4b9c635cf10c35c7
mr_illi <kimri42@naver.com>
2023-05-03 21:30:55 +0900

    Optional Positional Parameters 학습

--- function.dart
+++ function.dart
@@ -6,17 +6,23 @@ String sayHello(String name) => "hello $name nice to meet you!";
 num plus(num a, num b) => a + b;
 */
 
-/* # 3.1 Named Paramters */
+/* # 3.1 Named Paramters
 String sayHello({
   required String name,
   int age = 99,
   String country = 'wakanda',
 }) {
   return "Hello $name, you are $age, and you come from $country";
+} */
+
+/* # 3.3 Optional Positional Parameters */
+String sayHello(String name, int age, [String? country = 'cuba']) {
+  return "Hello $name, you are $age, and you come from $country";
 }
 
 void main() {
   // print(sayHello('illi', 20, 'cuba'));
-  print(sayHello(age: 20, country: 'cuba', name: 'illi'));
-  print(sayHello(name: 'dada')); // null safety
+  // print(sayHello(age: 20, country: 'cuba', name: 'illi'));
+  // print(sayHello(name: 'dada')); // null safety
+  print(sayHello('illi', 20));
 }
