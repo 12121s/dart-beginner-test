880ff262a1f935c3c2610d8fa6c7da54de0427a9
mr_illi <kimri42@naver.com>
2023-05-03 21:20:24 +0900

    Named Paramters 학습

--- function.dart
+++ function.dart
@@ -1,10 +1,22 @@
 /* # 3.0 Defining a Function
 String sayHello(String name) {
   return "hello $name nice to meet you!";
-} */
+} 
 String sayHello(String name) => "hello $name nice to meet you!";
 num plus(num a, num b) => a + b;
+*/
+
+/* # 3.1 Named Paramters */
+String sayHello({
+  required String name,
+  int age = 99,
+  String country = 'wakanda',
+}) {
+  return "Hello $name, you are $age, and you come from $country";
+}
 
 void main() {
-  print(sayHello("illi"));
+  // print(sayHello('illi', 20, 'cuba'));
+  print(sayHello(age: 20, country: 'cuba', name: 'illi'));
+  print(sayHello(name: 'dada')); // null safety
 }
