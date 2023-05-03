2689caa510488db26b7ecec4fd8e90b248e67fbb
mr_illi <kimri42@naver.com>
2023-05-03 21:39:04 +0900

    Q Operator(??, ?=) 학습

--- function.dart
+++ function.dart
@@ -15,14 +15,30 @@ String sayHello({
   return "Hello $name, you are $age, and you come from $country";
 } */
 
-/* # 3.3 Optional Positional Parameters */
+/* # 3.3 Optional Positional Parameters
 String sayHello(String name, int age, [String? country = 'cuba']) {
   return "Hello $name, you are $age, and you come from $country";
+} */
+
+/* # 3.4 QQ Operator ex) ??, ?= 
+String capitalizeName(String name) => name.toUpperCase();
+String capitalizeName(String name) {
+  if (name != null) return name.toUpperCase();
+  return 'anon';
 }
+String capitalizeName(String? name) =>
+    name != null ? name.toUpperCase() : 'anon'; */
+String capitalizeName(String? name) => name?.toUpperCase() ?? 'anon';
 
 void main() {
   // print(sayHello('illi', 20, 'cuba'));
   // print(sayHello(age: 20, country: 'cuba', name: 'illi'));
   // print(sayHello(name: 'dada')); // null safety
-  print(sayHello('illi', 20));
+  // print(sayHello('illi', 20));
+  capitalizeName('illi');
+  capitalizeName(null);
+
+  String? name;
+  name ??= 'illi';
+  print(name);
 }
