2c521f0c3959311c84bd2ed7e5b0eef421868aed
mr_illi <kimri42@naver.com>
2023-05-04 17:58:11 +0900

    class Named Constructor Parameters 학습

--- classes.dart
+++ classes.dart
@@ -2,24 +2,34 @@
 class Player {
   late final String name;
   late int xp = 999;
+  String team;
+  int age;
 
   /* # 4.1 Constructors */
   /* 생성자 선언
   Player(String name, int xp) {
     this.name = name;
     this.xp = xp;
-  } */
+  }
   Player(this.name, this.xp);
-
+ */
   void sayHello() {
     print("hello my name is $name");
   }
+
+  /* # 4.2 Named Constructor Parameters */
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
 }
 
 void main() {
-  var illi = Player("illi", 100);
-  illi.sayHello();
-
-  var dada = Player("dada", 1000);
-  dada.sayHello();
+  var illi = Player(
+    name: "illi",
+    xp: 100,
+    team: "blue",
+    age: 1,
+  );
 }
